1d8cd58b7b6ce405fe69851025f0c563937f715e
moses-mativo <clishtivo@gmail.com>
2022-08-08 22:14:22 +0300

    user authentication

---
+++ front-end/src/pages/LogInPage.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import { useToken } from '../auth/useToken';
+
+export const LogInPage = () => {
+    const [token, setToken] = useToken();
+
+    const [errorMessage, setErrorMessage] = useState(false);
+
+    const [emailValue, setEmailValue] = useState('');
+    const [passwordValue, setPasswordValue] = useState('');
+    useEffect(() => {
+        if (setErrorMessage) {
+            setTimeout(() => {
+                setErrorMessage(false);
+            }, 3000);
+        }
+    }, [errorMessage]);
+
+    const history = useHistory();
+
+    const onLogInClicked = async () => {
+        try{
+            const response = await axios.post('/api/login', {
+                email: emailValue,
+                password: passwordValue,
+            });
+            const { token } = response.data;
+            setToken(token);
+            setErrorMessage(false);
+            history.push('/');
+
+        } catch(e){
+            setErrorMessage(true);
+        }
+    }
+
+    return (
+        <div className="content-container">
+            <h1>Log In</h1>
+            {errorMessage && <div className="fail">Username or Password incorrect</div>}
+            <input
+                value={emailValue}
+                onChange={e => setEmailValue(e.target.value)}
+                placeholder="someone@gmail.com" />
+            <input
+                type="password"
+                value={passwordValue}
+                onChange={e => setPasswordValue(e.target.value)}
+                placeholder="password" />
+            <hr />
+            <button
+                disabled={!emailValue || !passwordValue}
+                onClick={onLogInClicked}>Log In</button>
+            <button onClick={() => history.push('/forgot-password')}>Forgot your password?</button>
+            <button onClick={() => history.push('/signup')}>Don't have an account? Sign Up</button>
+        </div>
+    );
+}
